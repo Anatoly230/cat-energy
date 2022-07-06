fedd79948f2b99596b0c0db175b2393c5acd3d27
Anatoly230 <https://github.com/Anatoly230>
2022-07-06 20:39:19 +0300

    добавил скрипт для тачскрина

--- source/js/miracles.js
+++ source/js/miracles.js
@@ -32,7 +32,41 @@ let hide = "miracles__pic--hide"
 
 rangeBar.addEventListener('click', function(e){
 
+console.log(e.target.value)
+if(e.target.classList.value === 'range-bar__strip'){
 
+  if(e.target.value<50){
+
+    pickItWas.style.width = e.target.value + '%';
+    pickBeCame.style.width = 100 - Number(e.target.value) + '%';
+
+  }else{
+
+    pickItWas.style.width = e.target.value + '%';
+    pickBeCame.style.width = 100 - Number(e.target.value) + '%';
+  }
+}
+
+if(rgBeCame.test(e.target.className)) {
+
+ if(!rgHide.test(pickItWas.className)){
+    pickItWas.classList.add(hide);
+    pickBeCame.classList.remove(hide);
+  }
+}else if(rgItWas.test(e.target.className)){
+
+    if(!rgHide.test(pickBeCame.className)){
+
+      pickBeCame.classList.add(hide);
+      pickItWas.classList.remove(hide);
+  }
+
+}
+})
+
+rangeBar.addEventListener('touchmove', function(e){
+
+console.log(e.target.value)
 if(e.target.classList.value === 'range-bar__strip'){
 
   if(e.target.value<50){
